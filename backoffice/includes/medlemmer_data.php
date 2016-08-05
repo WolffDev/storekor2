daf0fc75789e5f1c3c118fb99e5dbef2b071c292
WolffDev <info@davidwolff.dk>
2016-08-05 23:35:52 +0200

    working with paging

---
+++ backoffice/includes/medlemmer_data.php
@@ -0,0 +1,100 @@
+<?php
+/* Database connection start */
+$servername = "localhost";
+$username = "root";
+$password = "4100";
+$dbname = "storekor";
+
+$conn = mysqli_connect($servername, $username, $password, $dbname) or die("Connection failed: " . mysqli_connect_error());
+
+/* Database connection end */
+
+
+// storing  request (ie, get/post) global array to a variable
+$requestData= $_REQUEST;
+
+
+$columns = array(
+// datatable column index  => database column name
+	0 => 'fornavn',
+	1 => 'efternavn',
+	2 => 'brugernavn',
+  3 => 'telefon',
+  4 => 'stemme',
+  5 => 'bruger_rolle',
+  6 => 'bruger_status',
+  7 => 'dato_oprettet',
+  8 => 'app_status'
+);
+
+// getting total number records without any search
+$sql = "SELECT fornavn, efternavn, brugernavn, telefon, stemme, bruger_rolle, bruger_status, dato_oprettet, app_status ";
+$sql.=" FROM medlemmer";
+$query=mysqli_query($conn, $sql) or die("medlemmer_data.php: get medlemmer");
+$totalData = mysqli_num_rows($query);
+$totalFiltered = $totalData;  // when there is no search parameter then total number rows = total number filtered rows.
+
+
+if( !empty($requestData['search']['value']) ) {
+	// if there is a search parameter
+	$sql = "SELECT fornavn, efternavn, brugernavn, telefon, stemme, bruger_rolle, bruger_status, dato_oprettet, app_status ";
+	$sql.=" FROM medlemmer";
+	$sql.=" WHERE fornavn LIKE '".$requestData['search']['value']."%' ";    // $requestData['search']['value'] contains search parameter
+	$sql.=" OR efternavn LIKE '".$requestData['search']['value']."%' ";
+  $sql.=" OR brugernavn LIKE '".$requestData['search']['value']."%' ";
+  $sql.=" OR telefon LIKE '".$requestData['search']['value']."%' ";
+  $sql.=" OR stemme LIKE '".$requestData['search']['value']."%' ";
+  $sql.=" OR bruger_rolle LIKE '".$requestData['search']['value']."%' ";
+  $sql.=" OR bruger_status LIKE '".$requestData['search']['value']."%' ";
+  $sql.=" OR dato_oprettet LIKE '".$requestData['search']['value']."%' ";
+	$sql.=" OR app_status LIKE '".$requestData['search']['value']."%' ";
+	$query=mysqli_query($conn, $sql) or die("medlemmer_data.php: get medlemmer");
+	$totalFiltered = mysqli_num_rows($query); // when there is a search parameter then we have to modify total number filtered rows as per search result without limit in the query
+
+	$sql.=" ORDER BY ". $columns[$requestData['order'][0]['column']]."   ".$requestData['order'][0]['dir']."   LIMIT ".$requestData['start']." ,".$requestData['length']."   "; // $requestData['order'][0]['column'] contains colmun index, $requestData['order'][0]['dir'] contains order such as asc/desc , $requestData['start'] contains start row number ,$requestData['length'] contains limit length.
+	$query=mysqli_query($conn, $sql) or die("medlemmer_data.php: get medlemmer"); // again run query with limit
+
+} else {
+
+	$sql = "SELECT fornavn, efternavn, brugernavn, telefon, stemme, bruger_rolle, bruger_status, dato_oprettet, app_status ";
+	$sql.=" FROM medlemmer";
+	$sql.=" ORDER BY ". $columns[$requestData['order'][0]['column']]."   ".$requestData['order'][0]['dir']."   LIMIT ".$requestData['start']." ,".$requestData['length']."   ";
+	$query=mysqli_query($conn, $sql) or die("medlemmer_data.php: get medlemmer");
+
+}
+
+
+
+$data = array();
+while( $row=mysqli_fetch_array($query) ) {  // preparing an array
+  $dato_oprettet = $row['dato_oprettet'];
+  $date = date_create($dato_oprettet);
+  $date_show = date_format($date, 'd\. M - Y');
+
+	$nestedData=array();
+
+	$nestedData[] = $row["fornavn"];
+	$nestedData[] = $row["efternavn"];
+	$nestedData[] = $row["brugernavn"];
+  $nestedData[] = $row["telefon"];
+	$nestedData[] = $row["stemme"];
+	$nestedData[] = $row["bruger_rolle"];
+  $nestedData[] = $row["bruger_status"];
+	$nestedData[] = $date_show;
+	$nestedData[] = $row["app_status"];
+
+	$data[] = $nestedData;
+}
+
+
+
+$json_data = array(
+			"draw"            => intval( $requestData['draw'] ),   // for every request/draw by clientside , they send a number as a parameter, when they recieve a response/data they first check the draw number, so we are sending same number in draw.
+			"recordsTotal"    => intval( $totalData ),  // total number of records
+			"recordsFiltered" => intval( $totalFiltered ), // total number of records after searching, if there is no searching then totalFiltered = totalData
+			"data"            => $data   // total data array
+			);
+
+echo json_encode($json_data);  // send data as json format
+
+?>
--- backoffice/includes/view_all_members.php
+++ backoffice/includes/view_all_members.php
@@ -1,7 +1,8 @@
-<table class="striped highlight responsive-table" id="medlemmer_table">
+<table class="" id="medlemmer_table">
   <thead>
     <tr>
       <th data-field="id">Fornavn</th>
+      <th data-field="id">Efternavn</th>
       <th data-field="price">Brugernavn</th>
       <th data-field="id">Telefon</th>
       <th data-field="id">Stemme</th>
@@ -12,52 +13,52 @@
     </tr>
   </thead>
 
-  <tbody>
+  <!-- <tbody> -->
     <?php
-      global $conn;
-      $query = "SELECT * FROM medlemmer ORDER BY id DESC";
-      $select_users = mysqli_query($conn, $query);
-
-      while($row = mysqli_fetch_assoc($select_users )) {
-        $fornavn = $row['fornavn'];
-        $efternavn = $row['efternavn'];
-        $brugernavn = $row['brugernavn'];
-        $telefon = $row['telefon'];
-        $stemme = $row['stemme'];
-        $bruger_rolle = $row['bruger_rolle'];
-        $bruger_status = $row['bruger_status'];
-        $dato_oprettet = $row['dato_oprettet'];
-        $app_status = $row['app_status'];
-
-        echo "<tr>";
-        echo "<td>$fornavn $efternavn</td>";
-        echo "<td>$brugernavn</td>";
-        echo "<td>$telefon</td>";
-        echo "<td>$stemme</td>";
-        echo "<td>$bruger_rolle</td>";
-        echo "<td>$bruger_status</td>";
-        $date = date_create($dato_oprettet);
-        $date_show = date_format($date, 'd\. M - Y');
-        echo "<td>$date_show</td>";
-        if($app_status == 'ny' || $app_status == 'oprettet af bestyrrelsen') {
-          echo "<td>$app_status</td>";
-        }
-        echo "<tr>";
-      }
+      // global $conn;
+      // $query = "SELECT * FROM medlemmer ORDER BY id DESC";
+      // $select_users = mysqli_query($conn, $query);
+      //
+      // while($row = mysqli_fetch_assoc($select_users )) {
+      //   $fornavn = $row['fornavn'];
+      //   $efternavn = $row['efternavn'];
+      //   $brugernavn = $row['brugernavn'];
+      //   $telefon = $row['telefon'];
+      //   $stemme = $row['stemme'];
+      //   $bruger_rolle = $row['bruger_rolle'];
+      //   $bruger_status = $row['bruger_status'];
+      //   $dato_oprettet = $row['dato_oprettet'];
+      //   $app_status = $row['app_status'];
+      //
+      //   echo "<tr>";
+      //   echo "<td>$fornavn $efternavn</td>";
+      //   echo "<td>$brugernavn</td>";
+      //   echo "<td>$telefon</td>";
+      //   echo "<td>$stemme</td>";
+      //   echo "<td>$bruger_rolle</td>";
+      //   echo "<td>$bruger_status</td>";
+      //   $date = date_create($dato_oprettet);
+      //   $date_show = date_format($date, 'd\. M - Y');
+      //   echo "<td>$date_show</td>";
+      //   if($app_status == 'ny' || $app_status == 'oprettet af bestyrrelsen') {
+      //     echo "<td>$app_status</td>";
+      //   }
+      //   echo "<tr>";
+      // }
     ?>
-  </tbody>
+  <!-- </tbody> -->
 </table>
 <script type="text/javascript" language="javascript" >
 			$(document).ready(function() {
 				var dataTable = $('#medlemmer_table').DataTable( {
-					"processing": true,
+					"processing": false,
 					"serverSide": true,
 					"ajax":{
-						url :"employee-grid-data.php", // json datasource
+						url :"includes/medlemmer_data.php", // json datasource
 						type: "post",  // method  , by default get
 						error: function(){  // error handling
 							$(".employee-grid-error").html("");
-							$("#employee-grid").append('<tbody class="employee-grid-error"><tr><th colspan="3">No data found in the server</th></tr></tbody>');
+							$("#medlemmer_table").append('<tbody class="employee-grid-error"><tr><th colspan="3">No data found in the server</th></tr></tbody>');
 							$("#employee-grid_processing").css("display","none");
 
 						}
