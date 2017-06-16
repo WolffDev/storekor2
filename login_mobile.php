99984c445adcf4526761135ad66d7b29299fd5ad
David Wolff <info@davidwolff.dk>
2017-06-16 21:24:49 +0200

    changed some styling

--- login_mobile.php
+++ login_mobile.php
@@ -10,20 +10,20 @@
   <div class="row">
     <form class="col s12 center" action="backoffice/includes/login.php" method="post">
       <div class="row">
-        <div class="col 1"></div>
+        <div class="col s1"></div>
         <div class="input-field col s10">
           <input id="username" type="text" class="validate" name="username">
           <label for="username">Email</label>
         </div>
-        <div class="col 1"></div>
+        <div class="col s1"></div>
       </div>
       <div class="row center">
-        <div class="col 1"></div>
+        <div class="col s1"></div>
         <div class="input-field col s10">
           <input id="password" type="password" class="validate" name="password">
           <label for="password">Adgangskode</label>
         </div>
-        <div class="col 1"></div>
+        <div class="col s1"></div>
       </div>
       <div class="row center">
         <button class="btn waves-effect waves-light" type="submit" name="login">Login<i class="material-icons right">send</i></button>
