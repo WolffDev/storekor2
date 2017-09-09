2bbfb8efec1386c0698051d8ac783adbf3ac5f35
David Wolff <info@davidwolff.dk>
2017-09-09 20:02:09 +0200

    new files, dunnow where they are from

---
+++ backoffice/js/script (1).js
@@ -0,0 +1,61 @@
+$( document ).ready(function() {
+  $('.timepicker').pickatime({
+    autoclose: true,
+    twelvehour: false,
+    donetext: 'Færdig'
+  });
+
+  $('.scrollspy').scrollSpy();
+  $(window).scroll(function(){
+    var $el = $('.fixedElement');
+    var isPositionFixed = ($el.css('position') === 'fixed');
+    if ($(this).scrollTop() > 242 && !isPositionFixed){
+      $('.fixedElement').css({'position': 'fixed', 'top': '0px'});
+    }
+    if ($(this).scrollTop() < 242 && isPositionFixed)
+    {
+      $('.fixedElement').css({'position': 'static', 'top': '0px'});
+    }
+  });
+
+  $('.checkbox-info').on('click', function() {
+    var check_member = $(this).parent().parent().find("input");
+    var absence_id = check_member.data('absence_id');
+
+    if(check_member.prop('checked') === false) {
+      $.ajax
+      ({
+          url: "includes/json/absence.php",
+          data:
+            {
+              "absence": "update",
+              "absence_id": absence_id,
+              "absence_status": 0
+
+            },
+          type: "post",
+      });
+
+    } else if(check_member.prop('checked') === true) {
+
+      $.ajax
+      ({
+          url: "includes/json/absence.php",
+          data:
+            {
+              "absence": "update",
+              "absence_id": absence_id,
+              "absence_status": 1
+            },
+          type: "post"
+      });
+    }
+
+  });
+
+  $('#selectAllBoxes').click(function(e){
+    var table= $(e.target).closest('table');
+    $('td input:checkbox',table).prop('checked',this.checked);
+  });
+
+}); // <-- !document.ready -->
